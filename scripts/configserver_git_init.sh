fed080c19eb095df85698801931d993fb2fe88aa
DIMITREE <jimbo17011990@gmail.com>
2021-07-17 04:00:59 +0300

    updated README.MD

--- scripts/configserver_git_init.sh
+++ scripts/configserver_git_init.sh
@@ -2,7 +2,8 @@ cd $HOME
 mkdir config-repo
 cd config-repo
 git init .
-echo 'mq.settings.exchange.name = dts.ps.exchange
+echo 'spring.rabbitmq.port=5672
+mq.settings.exchange.name = dts.ps.exchange
 mq.settings.queue.name = dts.ps.queue
 mq.settings.purchase-key = dts.routing.purchase-key
 mq.settings.subscription-key = dts.routing.subscription-key' > application.properties
